e013568d20d7c3f14fe17a879d4c0c6cf715bc8e
platane <me@platane.me>
2017-07-30 00:35:11 +0200

    :rocket: set gameSpeed

--- src/action/index.js
+++ src/action/index.js
@@ -1,3 +1,12 @@
 import type { Action as Action_Game } from './game'
 
-export type Action = Action_Game
+type Action_GameSpeed_Set = {
+    type: 'game:gameSpeed:set',
+    gameSpeed: number,
+}
+export const setGameSpeed = (gameSpeed: number): Action_GameSpeed_Set => ({
+    type: 'game:gameSpeed:set',
+    gameSpeed,
+})
+
+export type Action = Action_Game | Action_GameSpeed_Set
