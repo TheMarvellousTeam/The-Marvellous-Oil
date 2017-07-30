ecbcebfd876cead43c099e54d28057fec85a5058
platane <me@platane.me>
2017-07-30 19:09:08 +0200

    :rocket: drop motion ( too ugly )

--- src/component/Bank/index.js
+++ src/component/Bank/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Motion, spring } from 'react-motion'
 
 import style from './style.css'
-
-import type { DrillClass } from '../../type'
 
 export type Props = {
     bank: number,
@@ -13,32 +10,16 @@ export type Props = {
 
 export const Bank = ({ bank, outFlow, inFlow }: Props) =>
     <div className={style.container}>
-        <Motion
-            defaultStyle={{ bank: 0 }}
-            style={{ bank: spring(bank, { stiffness: 50, damping: 10 }) }}
-        >
-            {v => {
-                const d = Math.min(1, Math.abs(v.bank - bank) / 10)
-                const u = Math.abs(0.5 - v.bank / 10 % 1) * 2
-                const s = 1 + u * d
-
-                return (
-                    <div
-                        className={style.container}
-                        style={{ transform: `scale(${s},${s})` }}
-                    >
-                        {Math.round(v.bank)}
-                    </div>
-                )
-            }}
-        </Motion>
+        <div className={style.bank}>
+            {bank}
+        </div>
 
         <div className={style.detail}>
-            <div className={style.outFlow}>
-                {-outFlow}
-            </div>
             <div className={style.inFlow}>
                 {inFlow}
+            </div>
+            <div className={style.outFlow}>
+                {-outFlow}
             </div>
         </div>
     </div>
